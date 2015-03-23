a270a208093b06b86458e1062fc3ea9824eab68f
Lei Chen <leichenscu@gmail.com>
2015-03-22 17:25:49 -0700

    Implemented CORS support

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
 
+  namespace :api do
+    resources :events, only: [:create]
+    match 'events.json' => 'events#create', via: :options, as: :events_options
+  end
+
 end
